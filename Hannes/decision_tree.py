034a07909474b09e0504efc9970297692ed54620
Hannes Bornemann <hannes.bornemann@outlook.de>
2023-12-16 22:52:18 +0100

    decision tree basic

--- Hannes/decision_tree.py
+++ Hannes/decision_tree.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 # Get the data
 col_names = ['contour number', 'aspect ratio', 'extent', 'Blue', 'Green', 'Red', 'Hue']
-# data = pd.read_csv("iris.csv", skiprows=1, header=None, names=col_names)
-data = pd.read_excel("output.xlsx", skiprows=1, header=None, names=col_names)
+data = pd.read_csv("output.csv", skiprows=1, header=None, names=col_names)
+# data = pd.read_excel("output.xlsx", skiprows=1, header=None, names=col_names)
 print(data.head(10))
 
 # Node class
--- Hannes/feature_extraction.py
+++ Hannes/feature_extraction.py
@@ -4,53 +4,95 @@ import numpy as np
 import os
 import feature_extraction_functions
 
-excel_layout = {
-    'contour number': [],
-    'aspect ratio': [],
-    'extent': [],
-    'Blue': [],
-    'Green': [],
-    'Red': [],
-    'Hue': [],
-    }
-input_dir = 'photos/Karotte'
-output_dir = 'photos_reduced/Karotte_reduced'
-files = os.listdir(input_dir)   # speichert dateinamen in liste
+df_layout = {
+    "contour number": [],
+    "aspect ratio": [],
+    "extent": [],
+    "Blue": [],
+    "Green": [],
+    "Red": [],
+    "Hue": [],
+    "class": [],
+}
+df = pd.DataFrame(df_layout)  # Erstellt Dataframe layout
 
-new_width, new_height = (125, 125) # (gewünschte Auflösung (Breite, Höhe) des bildes für die Merkmalerkennung)
-zoomfactor = int(600 / max(new_width, new_height)) # zahl 600 ist die größte seite des Anzeigefensters
-zoom = (new_width*zoomfactor, new_height*zoomfactor) # Berechnung des Zoomfaktors (nur für Anzeige)
+new_width, new_height = (
+    125,
+    125,
+)  # (gewünschte Auflösung (Breite, Höhe) des bildes für die Merkmalerkennung)
+zoomfactor = int(
+    600 / max(new_width, new_height)
+)  # zahl 600 ist die größte seite des Anzeigefensters
+zoom = (
+    new_width * zoomfactor,
+    new_height * zoomfactor,
+)  # Berechnung des Zoomfaktors (nur für Anzeige)
 
-max_files = 20  # Anzahl bilder die gelesen werden sollen
-count = 1
 
-for file in files:
-    # Bild aus Ordner einlesen, verkleinern in Zielordner schreiben und zurück geben
-    img = feature_extraction_functions.resize(input_dir, output_dir, file, new_width, new_height)
+for i in range(3):
+    if i == 0:
+        input_dir = "photos/Zwiebel"
+        output_dir = "photos_reduced/Zwiebel_reduced"
+        files = os.listdir(input_dir)  # speichert dateinamen in liste
+        count = 1  # setzten
+        max_files = 50  # Anzahl bilder die gelesen werden sollen
+    elif i == 1:
+        input_dir = "photos/Karotte"
+        output_dir = "photos_reduced/Karotte_reduced"
+        files = os.listdir(input_dir)
+        count = 51  # zurücksetzten (nur Reihe fortsetzen wenn bilder angezeigt werden sollen, weil bilder sich sonst überschreiben. Sonst kann wieder bei 1 begoonen werden und immer gleiches "max_files" genutzt werden)
+        max_files = 100  # Anzahl bilder die gelesen werden sollen
+    elif i == 2:
+        input_dir = "photos/Kartoffel"
+        output_dir = "photos_reduced/Kartoffel_reduced"
+        files = os.listdir(input_dir)
+        count = 101
+        max_files = 150
 
-    # finale Merkmale:
-    # contour_number = feature_extraction_functions.contour_number(img, count, zoom)
-    # average_blue, average_green, average_red, average_hue = feature_extraction_functions.mean_colours(img, count, zoom)
-    # extent, aspect_ratio = feature_extraction_functions.extent(img, count, zoom)
+    for file in files:
+        # Bild aus Ordner einlesen, verkleinern in Zielordner schreiben und zurück geben
+        img = feature_extraction_functions.resize(
+            input_dir, output_dir, file, new_width, new_height
+        )
 
-    contour_number, aspect_ratio, extent, average_blue, average_green, average_red , average_hue = feature_extraction_functions.get_Features(img, count, zoom)
+        # finale Merkmale:
+        # contour_number = feature_extraction_functions.contour_number(img, count, zoom)
+        # average_blue, average_green, average_red, average_hue = feature_extraction_functions.mean_colours(img, count, zoom)
+        # extent, aspect_ratio = feature_extraction_functions.extent(img, count, zoom)
 
-    # werte an value im dictionary hängen
-    excel_layout['contour number'].append(contour_number)
-    excel_layout['aspect ratio'].append(aspect_ratio)
-    excel_layout['extent'].append(extent)
-    excel_layout['Blue'].append(average_blue)
-    excel_layout['Green'].append(average_green)
-    excel_layout['Red'].append(average_red)
-    excel_layout['Hue'].append(average_hue)
-    
-    if count >= max_files:
-        break
-    count += 1
-    
+        (
+            contour_number,
+            aspect_ratio,
+            extent,
+            average_blue,
+            average_green,
+            average_red,
+            average_hue,
+        ) = feature_extraction_functions.get_Features(img, count, zoom)
 
-df = pd.DataFrame(excel_layout) 
-df.to_csv('output.csv', sep=';') # csv speichert weniger Metadaten, braucht weniger Speicher, sehr sinnvoll wenn Daten iwann sehr groß werden 
+        # werte an value im dictionary hängen
+        df_layout["contour number"].append(contour_number)
+        df_layout["aspect ratio"].append(aspect_ratio)
+        df_layout["extent"].append(extent)
+        df_layout["Blue"].append(average_blue)
+        df_layout["Green"].append(average_green)
+        df_layout["Red"].append(average_red)
+        df_layout["Hue"].append(average_hue)
+        df_layout["class"].append(i)
+
+        if count >= max_files:
+            break
+        count += 1
+
+
+df = pd.DataFrame(df_layout)
+
+# df = df._append(pd.DataFrame(df_layout), ignore_index=True)  # neue Zeilen an df anhängen, Indexierung fortführen
+
+
+df.to_csv(
+    "output.csv", sep=","
+)  # csv speichert weniger Metadaten, braucht weniger Speicher, sehr sinnvoll wenn Daten iwann sehr groß werden. "," als trennzeichen für ordnetliche formatierung der anzeigetabelle
 
 cv.waitKey(0)
-cv.destroyAllWindows() 
+cv.destroyAllWindows()
